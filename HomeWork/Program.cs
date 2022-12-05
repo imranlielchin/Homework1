705b5e042e9ef180934c8b8b388a9e2cc00bc85e
imranlielchin <elchinii@code.edu.az>
2022-12-05 19:52:29 +0400

    init

---
+++ HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //string reqem = Console.ReadLine(); 
+            //int firstNumber = int.Parse(reqem);
+            //string ikinciReqem = Console.ReadLine();
+            //int secondNumber = int.Parse(ikinciReqem);
+            //Console.WriteLine(firstNumber + secondNumber);
+            
+
+            string a = Console.ReadLine();
+            int firstNumber = int.Parse(a);
+            string b = Console.ReadLine();
+            int secondNumber = int.Parse(b);
+            Console.WriteLine(firstNumber % secondNumber);
+        }
+    }
+}
